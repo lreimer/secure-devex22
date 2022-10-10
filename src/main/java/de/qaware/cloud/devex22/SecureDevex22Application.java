02d79ebe2de0735767028a8811fadb587883a618
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2022-10-10 08:16:05 +0200

    Initial service skeleton added.

---
+++ src/main/java/de/qaware/cloud/devex22/SecureDevex22Application.java
@@ -0,0 +1,13 @@
+package de.qaware.cloud.devex22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureDevex22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureDevex22Application.class, args);
+	}
+
+}
---
+++ src/test/java/de/qaware/cloud/devex22/SecureDevex22ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.qaware.cloud.devex22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureDevex22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
