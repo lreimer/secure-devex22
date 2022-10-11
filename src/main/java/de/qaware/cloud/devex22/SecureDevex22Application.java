17437218e2f297d4bcd7852cebae27b5fc0dfca7
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2022-10-11 10:41:38 +0200

    Test.

--- src/main/java/de/qaware/cloud/devex22/SecureDevex22Application.java
+++ src/main/java/de/qaware/cloud/devex22/SecureDevex22Application.java
@@ -1,5 +1,8 @@
 package de.qaware.cloud.devex22;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +11,6 @@ public class SecureDevex22Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecureDevex22Application.class, args);
-	}
+		}
 
 }
